b7dd39bfab252f2db4ec0232b15d734f22d20732
Muhamed ibrahim <92036859+Mohammadibbu@users.noreply.github.com>
2024-10-07 09:34:27 +0530

    Update firebaseinitialization.js

--- source/firebaseinitialization.js
+++ source/firebaseinitialization.js
@@ -29,16 +29,27 @@ import {
 //   messagingSenderId: "56987502786",
 //   appId: "1:56987502786:web:fdf8ea92b2f14bdec1935b",
 // };
+//nc app
 const firebaseConfig = {
-  apiKey: "AIzaSyCB9l1rMhRBvhzZpGXjlgEqfrWczfhoJxI",
-  authDomain: "wc01-d8bcc.firebaseapp.com",
-  databaseURL: "https://wc01-d8bcc-default-rtdb.firebaseio.com",
-  projectId: "wc01-d8bcc",
-  storageBucket: "wc01-d8bcc.appspot.com",
-  messagingSenderId: "190367232875",
-  appId: "1:190367232875:web:ca86d79e9f844c50b5522e",
-  measurementId: "G-272GDMX5T4"
-};
+    apiKey: "AIzaSyAUo5mq9Od16XAQY4eey7qFnkpwhI2iy1w",
+    authDomain: "nc-app1.firebaseapp.com",
+    databaseURL: "https://nc-app1-default-rtdb.firebaseio.com",
+    projectId: "nc-app1",
+    storageBucket: "nc-app1.appspot.com",
+    messagingSenderId: "851652831721",
+    appId: "1:851652831721:web:c30e82e904e7a863810378",
+    measurementId: "G-6RE1YN8ZQ3"
+  };
+//const firebaseConfig = {
+  //apiKey: "AIzaSyCB9l1rMhRBvhzZpGXjlgEqfrWczfhoJxI",
+ // authDomain: "wc01-d8bcc.firebaseapp.com",
+ // databaseURL: "https://wc01-d8bcc-default-rtdb.firebaseio.com",
+//  projectId: "wc01-d8bcc",
+ // storageBucket: "wc01-d8bcc.appspot.com",
+ // messagingSenderId: "190367232875",
+  //appId: "1:190367232875:web:ca86d79e9f844c50b5522e",
+  //measurementId: "G-272GDMX5T4"
+//};
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
